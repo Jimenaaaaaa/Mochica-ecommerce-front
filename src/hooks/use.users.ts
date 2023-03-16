bba39ba658e3fdd4482a4e598cdc35f289f00e47
Jimena <jimenanrp2@gmail.com>
2023-03-16 20:10:58 +0100

    Edit custom hook

--- src/hooks/use.users.ts
+++ src/hooks/use.users.ts
@@ -1,36 +1,41 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { LoginData, RegisterData } from "../models/models/user";
-import { login } from "../reducers/slice";
+import { loginSlice } from "../reducers/user.slice";
+
 import { UserRepo } from "../services/users.api.repo";
 
-import { AppDispatch } from "../store/store";
+import { AppDispatch, RootState } from "../store/store";
 
 export function useUsers(repo: UserRepo) {
+  const users = useSelector((state: RootState) => state.users);
   const dispatch = useDispatch<AppDispatch>();
 
-  const userLogin = async (info: LoginData) => {
+  const login = async (info: LoginData) => {
     try {
       const data = await repo.loginUser(info);
       // Mirar si cambio lo que devuelve el back
-      dispatch(login(data));
+      dispatch(loginSlice(data));
     } catch (error) {
+      // gestionar errores
       console.error((error as Error).message);
     }
   };
 
-  const userRegister = async (info: RegisterData) => {
+  const register = async (info: RegisterData) => {
     try {
       const data = await repo.registerUser(info);
       // Mirar si cambio lo que devuelve el back
-      dispatch(login(data));
+      dispatch(loginSlice(data));
+      //
     } catch (error) {
       console.error((error as Error).message);
     }
   };
 
   return {
-    userLogin,
-    userRegister,
+    users,
+    login,
+    register,
   };
 }
 
