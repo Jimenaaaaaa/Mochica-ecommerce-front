8bbe99514b045809551087b0de82f14fe63d654d
Jimena <jimenanrp2@gmail.com>
2023-03-16 20:10:19 +0100

    Edit login component with custom hook

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,26 +1,26 @@
-import { SyntheticEvent } from "react";
-import { useDispatch } from "react-redux";
+/* eslint-disable jsx-a11y/no-redundant-roles */
+import { SyntheticEvent, useMemo } from "react";
 import { Link } from "react-router-dom";
+import { useUsers } from "../../hooks/use.users";
 import { LoginData } from "../../models/models/user";
-import { login } from "../../reducers/slice";
+
 import { UserRepo } from "../../services/users.api.repo";
-import { AppDispatch } from "../../store/store";
+
 import styles from "./login.module.scss";
 
 export default function Login() {
-  const dispatch = useDispatch<AppDispatch>();
-  const repo = new UserRepo();
+  const repo = useMemo(() => new UserRepo(), []);
+  const { login } = useUsers(repo);
   const handleSubmit = async (ev: SyntheticEvent<HTMLFormElement>) => {
     ev.preventDefault();
     const formElement = ev.currentTarget;
     const formData: LoginData = {
-      email: (formElement[0] as HTMLFormElement).value,
-      password: (formElement[1] as HTMLFormElement).value,
+      email: (formElement.elements[0] as HTMLFormElement).value,
+      password: (formElement.elements[1] as HTMLFormElement).value,
     };
 
-    const successLoginData = await repo.loginUser(formData);
-
-    dispatch(login(successLoginData));
+    login(formData);
+    formElement.reset();
   };
 
   return (
@@ -28,16 +28,21 @@ export default function Login() {
       <div className={styles.enter}>
         <div className={styles.enter_options}>
           <Link to={`/login`}>
-            <div>Login</div>
+            <h2>Login</h2>
           </Link>
           <Link to={`/register`}>
-            <div>Register</div>
+            <h2>Register</h2>
           </Link>
         </div>
         <div className={styles.login}>
           <form action="" className={styles.login_form} onSubmit={handleSubmit}>
             <input type="text" placeholder="EMAIL" required />
-            <input type="password" placeholder="PASSWORD" required />
+            <input
+              type="password"
+              placeholder="PASSWORD"
+              role="textbox"
+              required
+            />
             <button>Submit</button>
           </form>
         </div>
