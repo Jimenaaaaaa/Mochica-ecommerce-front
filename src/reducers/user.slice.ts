8d13a63d0da8ebba9225d708f504e77b1d9de114
Jimena <jimenanrp2@gmail.com>
2023-03-16 20:11:14 +0100

    Add user.slice file

---
+++ src/reducers/user.slice.ts
@@ -0,0 +1,53 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from "../models/models/user";
+
+// Ver si sumar mas tarde:
+// type Status = "loading" | "idle" | "error";
+// type State = {
+//   userLoggingStatus: Status;
+//   userLogged: {
+//     token: string;
+//     user: User;
+//   } | null;
+//   user: User;
+// };
+
+export type LoginData = {
+  token: string;
+  user: User;
+};
+
+export type State = {
+  loggedUser:
+    | {
+        token: string | null;
+        user: User;
+      }
+    | {};
+  role: string | null;
+};
+
+const initialState: State = {
+  loggedUser: {
+    token: null,
+    user: {},
+  },
+  role: "user",
+};
+
+const slice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginSlice(state, action: PayloadAction<LoginData>) {
+      // Revisar el payload
+      state.loggedUser = {
+        token: action.payload.token,
+        user: action.payload.user,
+      };
+    },
+  },
+});
+
+export const { loginSlice } = slice.actions;
+export const usersReducer = slice.reducer;
