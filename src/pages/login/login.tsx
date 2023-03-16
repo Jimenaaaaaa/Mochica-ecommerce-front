510484d643f4907198d74f36a71477ab9d6a3ec1
Jimena <jimenanrp2@gmail.com>
2023-03-16 11:51:56 +0100

    Add handleSubmit

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,20 +1,43 @@
+import { SyntheticEvent } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { LoginData } from "../../models/models/user";
+import { login } from "../../reducers/slice";
+import { UserRepo } from "../../services/users.api.repo";
+import { AppDispatch } from "../../store/store";
 import styles from "./login.module.scss";
 
 export default function Login() {
-  // Decir que lea si el usuario se quiere registar o no y cambiar el formulario. Maybe
-  // Poniendolo en el estado?
+  const dispatch = useDispatch<AppDispatch>();
+  const repo = new UserRepo();
+  const handleSubmit = async (ev: SyntheticEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+    const formElement = ev.currentTarget;
+    const formData: LoginData = {
+      email: (formElement[0] as HTMLFormElement).value,
+      password: (formElement[1] as HTMLFormElement).value,
+    };
+
+    const successLoginData = await repo.loginUser(formData);
+
+    dispatch(login(successLoginData));
+  };
 
   return (
     <>
       <div className={styles.enter}>
         <div className={styles.enter_options}>
-          <div>Login</div>
-          <div>Register</div>
+          <Link to={`/login`}>
+            <div>Login</div>
+          </Link>
+          <Link to={`/register`}>
+            <div>Register</div>
+          </Link>
         </div>
         <div className={styles.login}>
-          <form action="" className={styles.login_form}>
+          <form action="" className={styles.login_form} onSubmit={handleSubmit}>
             <input type="text" placeholder="EMAIL" required />
-            <input type="text" placeholder="PASSWORD" required />
+            <input type="password" placeholder="PASSWORD" required />
             <button>Submit</button>
           </form>
         </div>
