86af6c7b39681607748e5dfecdbd21457c88f0b1
Jimena <jimenanrp2@gmail.com>
2023-03-21 16:07:26 +0100

    Change duplicated lines

--- src/hooks/use.products.test.tsx
+++ src/hooks/use.products.test.tsx
@@ -30,7 +30,6 @@ describe("Given the useProducts Custom Hook, an ApiRepo and a given component", 
         useProducts(mockRepo);
       return (
         <>
-          {/* Meter el get by tag */}
           <button onClick={() => productsGetAll()}>getAll</button>
           <button onClick={() => productsGetById(mockId)}>getById</button>
           <button onClick={() => productsGetByTag("tag")}>getByTag</button>
@@ -46,21 +45,14 @@ describe("Given the useProducts Custom Hook, an ApiRepo and a given component", 
     );
   });
   describe("When the TestComponent is rendered", () => {
-    test("Then, the button should be in the document", async () => {
+    test("Then, the button must be in the document", async () => {
       const elements = await screen.findAllByRole("button");
       expect(elements[0]).toBeInTheDocument();
     });
   });
-  // Ver si pongo estas lineas despues, cuando reviste el custom
-  // describe("When the TestComponent is rendered and the register button is clicked", () => {
-  //   test("Then, the ProductRegister function should be called", async () => {
-  //     const elements = await screen.findAllByRole("button");
-  //     await act(async () => ProductEvent.click(elements[0]));
-  //     expect(mockRepo.registerProduct).toHaveBeenCalled();
-  //   });
 
-  describe("When the TestComponent is rendered and the GetAll button is clicked", () => {
-    test("Then, the productsGetAll function should be called", async () => {
+  describe("When the TestComponent is showed and the GetAll button is clicked", () => {
+    test("Then, the productsGetAll method should be called", async () => {
       const elements = await screen.findAllByRole("button");
       await fireEvent.click(elements[0]);
       expect(mockRepo.getAll).toHaveBeenCalled();
@@ -69,14 +61,14 @@ describe("Given the useProducts Custom Hook, an ApiRepo and a given component", 
 
   describe("When the TestComponent is rendered and the getById button is clicked", () => {
     test("Then, the productsgetById function should be called", async () => {
-      const elements = await screen.findAllByRole("button");
-      await fireEvent.click(elements[1]);
+      const buttons = await screen.findAllByRole("button");
+      await fireEvent.click(buttons[1]);
       expect(mockRepo.getById).toHaveBeenCalled();
     });
   });
 
-  describe("When the TestComponent is rendered and the getByTag button is clicked", () => {
-    test("Then, the productsgetByTag function should be called", async () => {
+  describe("When the getByTag button is clicked", () => {
+    test("Then, the productsgetByTag function should have been called", async () => {
       const elements = await screen.findAllByRole("button");
       await fireEvent.click(elements[2]);
       expect(mockRepo.getByTag).toHaveBeenCalled();
