16a68d46767d31483631d83fc6e99344738528ed
Jimena <jimenanrp2@gmail.com>
2023-03-22 18:59:36 +0100

    Add reducer to slice

--- src/reducers/product.slice.ts
+++ src/reducers/product.slice.ts
@@ -3,32 +3,50 @@ import { Product } from "../models/product";
 
 export type ProductState = {
   selectedProduct: Product;
-  Products: Product[];
+  products: Product[];
 };
 
 const initialState: ProductState = {
   selectedProduct: {} as Product,
-  Products: [],
+  products: [],
 };
 
 export const ProductSlice = createSlice({
-  name: "Product",
+  name: "product",
   initialState,
   reducers: {
     getAllProducts(state, action: PayloadAction<Product[]>) {
-      state.Products = action.payload;
+      state.products = action.payload;
     },
-    getByTagProducts(state, action: PayloadAction<Product>) {
-      state.Products = [action.payload];
+    getByTagProducts(state, action: PayloadAction<Product[]>) {
+      state.products = action.payload;
     },
     getProductById(state, action: PayloadAction<Product>) {
       state.selectedProduct = action.payload;
     },
-    // Falta añadir si se selecciona 1 sola Product
+    patchProduct(state, action: PayloadAction<Product>) {
+      state.products = state.products.map((item) =>
+        item.id === action.payload.id ? action.payload : item
+      );
+    },
+    postProduct(state, action: PayloadAction<Product>) {
+      state.products = [...state.products, action.payload];
+    },
+    deleteProduct(state, action: PayloadAction<string>) {
+      state.products = state.products.filter(
+        (product) => product.id !== action.payload
+      );
+    },
   },
 });
 
-export const { getAllProducts, getByTagProducts, getProductById } =
-  ProductSlice.actions;
+export const {
+  getAllProducts,
+  getByTagProducts,
+  getProductById,
+  patchProduct,
+  postProduct,
+  deleteProduct,
+} = ProductSlice.actions;
 
 export const productsReducer = ProductSlice.reducer;
