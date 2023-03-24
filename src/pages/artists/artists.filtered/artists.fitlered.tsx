b1d9648fcb949ad3adacdc172acd2f94b20e3beb
Jimena <jimenanrp2@gmail.com>
2023-03-24 09:47:32 +0100

    Add filtered artists component

---
+++ src/pages/artists/artists.filtered/artists.fitlered.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useMemo } from "react";
+import { useParams } from "react-router-dom";
+import { Card } from "../../../components/card/card";
+import { useProducts } from "../../../hooks/use.products";
+import { Product } from "../../../models/product";
+import { ProductsRepo } from "../../../services/products/products.api.repo";
+import styles from "./artists.filtered.module.scss";
+
+export default function ArtistsFiltered() {
+  const { author } = useParams();
+  const repo = useMemo(() => new ProductsRepo(), []);
+  const { products, productsGetAll } = useProducts(repo);
+
+  useEffect(() => {
+    productsGetAll();
+  }, [productsGetAll]);
+
+  const selected = products.products.filter(
+    (item) => item.author === author && item
+  );
+
+  return (
+    <div data-testid="cardlist-component">
+      <ul className={styles.card_list}>
+        {selected.map((item: Product) => (
+          <Card product={item} key={item.id}></Card>
+        ))}
+      </ul>
+      {/* Las flechas de next page y previous page */}
+    </div>
+  );
+}
