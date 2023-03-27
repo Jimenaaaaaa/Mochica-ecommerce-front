43171fc7e9b9d2c24cbc2c4bb8555ed423a47aaa
Jimena <jimenanrp2@gmail.com>
2023-03-27 15:55:13 +0200

    Add new test case

--- src/pages/details/details.test.tsx
+++ src/pages/details/details.test.tsx
@@ -1,13 +1,22 @@
 /* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable testing-library/no-render-in-setup */
-import { render, screen, act } from "@testing-library/react";
-import { Provider } from "react-redux";
+import { act, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 import { useProducts } from "../../hooks/use.products";
-import { store } from "../../store/store";
+import { ProductsRepo } from "../../services/products/products.api.repo";
 
 import Details from "./details";
 jest.mock("../../hooks/use.products");
+
+let mockRepo = {
+  getAll: jest.fn(),
+  getByTag: jest.fn(),
+  getById: jest.fn(),
+  patch: jest.fn(),
+  post: jest.fn(),
+  delete: jest.fn(),
+} as unknown as ProductsRepo;
 
 describe("Given Details page component", () => {
   beforeEach(async () => {
@@ -16,15 +25,14 @@ describe("Given Details page component", () => {
         selectedProduct: {},
         Products: [],
       },
+      productDelete: jest.fn(),
     });
 
     await act(async () => {
       render(
-        <Provider store={store}>
-          <MemoryRouter>
-            <Details></Details>
-          </MemoryRouter>
-        </Provider>
+        <MemoryRouter>
+          <Details></Details>
+        </MemoryRouter>
       );
     });
   });
@@ -33,6 +41,15 @@ describe("Given Details page component", () => {
     test('Then, the title "Details" should be in the document', async () => {
       const element = screen.getAllByRole("heading");
       expect(element[0]).toBeInTheDocument();
+    });
+  });
+
+  describe("When click the button delete", () => {
+    test("Then productDelete should have been called", async () => {
+      const element = screen.getAllByRole("button");
+      expect(element[1]).toBeInTheDocument();
+      await userEvent.click(element[1]);
+      expect(useProducts(mockRepo).productDelete).toHaveBeenCalled();
     });
   });
 });
