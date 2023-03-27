188805e3008e197c4fa086534baf762f08456b1f
Jimena <jimenanrp2@gmail.com>
2023-03-27 15:49:01 +0200

    Add firebase source to image

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,11 @@ export function Header() {
     <>
       <NavBar></NavBar>
       <div className={styles.title}>
-        <img className={styles.title_image} src="mochica.png" alt="" />
+        <img
+          className={styles.title_image}
+          src="https://firebasestorage.googleapis.com/v0/b/mochica-ceramics.appspot.com/o/mochica?alt=media&token=9b8072a0-c452-4aa7-8b8c-0d0015778a22"
+          alt="mochica"
+        />
       </div>
     </>
   );
