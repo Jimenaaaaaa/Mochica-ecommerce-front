d7b5fcd79ea09439ff91d2f20b9db02bb776ae32
Jimena <jimenanrp2@gmail.com>
2023-03-27 15:47:57 +0200

    Add test case

--- src/components/cardlist/cardlist.test.tsx
+++ src/components/cardlist/cardlist.test.tsx
@@ -1,15 +1,26 @@
 /* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable testing-library/no-render-in-setup */
 import { act, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
 import { useProducts } from "../../hooks/use.products";
 import { Product } from "../../models/product";
+import { ProductsRepo } from "../../services/products/products.api.repo";
 import { store } from "../../store/store";
 import Cardlist from "./cardlist";
 
 jest.mock("../../hooks/use.products");
 jest.mock("../card/card");
+
+let mockRepo = {
+  getAll: jest.fn(),
+  getByTag: jest.fn(),
+  getById: jest.fn(),
+  patch: jest.fn(),
+  post: jest.fn(),
+  delete: jest.fn(),
+} as unknown as ProductsRepo;
 
 describe("Given Product List component", () => {
   beforeEach(async () => {
@@ -25,11 +36,13 @@ describe("Given Product List component", () => {
             name: "test2",
           } as Product,
         ],
+        currentPage: 2,
       },
       productsGetAll: jest.fn(),
+      productsGetById: jest.fn(),
     });
 
-    await act(() => {
+    await act(async () => {
       render(
         <Provider store={store}>
           <MemoryRouter>
@@ -41,16 +54,27 @@ describe("Given Product List component", () => {
   });
 
   describe("When the Card is rendered", () => {
-    test("Then it should return images", async () => {
-      act(async () => {
-        const elements = await screen.findAllByRole("img");
-        expect(elements[0]).toBeInTheDocument();
+    test("Then it should return the list of cards", async () => {
+      await act(async () => {
         const name = await screen.findByRole("list");
         expect(name).toBeInTheDocument();
         const elementNumbers = screen.getAllByRole("list");
         for (let i = 0; i < elementNumbers.length; i++) {
           expect(elementNumbers).toBeTruthy();
         }
+      });
+    });
+  });
+
+  describe("When the user clicks on the pagination buttons", () => {
+    test("Then it should call the productsGetAll method", async () => {
+      await act(async () => {
+        const buttons = screen.getAllByRole("button");
+        expect(buttons[0]).toBeInTheDocument();
+        expect(buttons[1]).toBeInTheDocument();
+        await userEvent.click(buttons[0]);
+        await userEvent.click(buttons[1]);
+        expect(useProducts(mockRepo).productsGetAll).toHaveBeenCalled();
       });
     });
   });
