043ccbdd3b0f9769d664f0779a00b6af28dd6e74
Jimena <jimenanrp2@gmail.com>
2023-03-28 19:20:21 +0200

    Edit product component

--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -1,9 +1,18 @@
+import { useParams } from "react-router-dom";
 import Cardlist from "../../components/cardlist/cardlist";
+import styles from "./products.module.scss";
 
 export default function Products() {
+  const { filter } = useParams();
+
   return (
-    <div>
-      <h1>Products</h1>
+    <div className={styles.product_container}>
+      {filter === "all" ? (
+        <h1 className={styles.products}>Products</h1>
+      ) : (
+        <h1 className={styles.products}>{filter}</h1>
+      )}
+
       <Cardlist></Cardlist>
     </div>
   );
