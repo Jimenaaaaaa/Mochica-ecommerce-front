8858c031cc1e5a84380109d1531a89570674616b
Jimena <jimenanrp2@gmail.com>
2023-03-28 19:22:45 +0200

    Erase unused code

--- src/services/users/users.api.repo.ts
+++ src/services/users/users.api.repo.ts
@@ -1,16 +1,10 @@
-import {
-  LoginData,
-  ProtoUser,
-  RegisterData,
-  User,
-} from "../../models/user";
+import { LoginData, ProtoUser, RegisterData, User } from "../../models/user";
 
-// Tengo que seguir rellenando el repo con el resto de metodos.
 
 export interface UserRepoStructure {
   loadUser(): Promise<User>;
   loginUser(user: LoginData): Promise<LoginData>;
-  registerUser(user: ProtoUser): Promise<User>; // Tb se puede llamar REGISTER
+  registerUser(user: ProtoUser): Promise<User>; 
 }
 
 export class UserRepo {
@@ -33,7 +27,7 @@ export class UserRepo {
       throw new Error(`Error http: ${resp.status} ${resp.statusText}`);
 
     const data = await resp.json();
-    return data; //Devuelve un objeto con el token
+    return data;
   }
 
   async registerUser(user: RegisterData) {
@@ -50,6 +44,5 @@ export class UserRepo {
       throw new Error(`Error http: ${resp.status} ${resp.statusText}`);
 
     const data = await resp.json();
-    return data.results; //Devuelve un objeto con el token
-  }
+    return data.results;
 }
