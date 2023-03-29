4dc2867d39c6f481779130028162f59f28ce727c
Jimena <jimenanrp2@gmail.com>
2023-03-30 01:40:06 +0200

    Fix cart component test

--- src/pages/cart/cart.test.tsx
+++ src/pages/cart/cart.test.tsx
@@ -1,11 +1,39 @@
-import { render, screen } from "@testing-library/react";
+/* eslint-disable testing-library/no-unnecessary-act */
+/* eslint-disable testing-library/no-render-in-setup */
+import { act, render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
+import { useUsers } from "../../hooks/use.users";
 import { store } from "../../store/store";
 import Cart from "./cart";
 
+jest.mock("../../hooks/use.users");
+
 describe("Given the Cart component", () => {
-  describe("When it is rendered", () => {
+  beforeEach(async () => {
+    (useUsers as jest.Mock).mockReturnValue({
+      users: {
+        loggedUser: {
+          token: "123",
+          user: {
+            cart: [],
+          },
+        },
+      },
+      addToCart: jest.fn(),
+    });
+
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <MemoryRouter>
+            <Cart></Cart>
+          </MemoryRouter>
+        </Provider>
+      );
+    });
+  });
+  describe("Given Details page component", () => {
     test("Then it should appear in the document", () => {
       render(
         <Provider store={store}>
@@ -14,8 +42,8 @@ describe("Given the Cart component", () => {
           </MemoryRouter>
         </Provider>
       );
-      const title = screen.getByRole("heading");
-      expect(title).toBeInTheDocument();
+      const title = screen.getAllByRole("heading");
+      expect(title[0]).toBeInTheDocument();
     });
   });
 });
